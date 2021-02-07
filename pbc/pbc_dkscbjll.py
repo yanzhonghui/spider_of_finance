049f11307176d0e5d55be1f5e72d62ff270a9e0f
yanzhonghui <admin@yanzhonghui.cn>
2021-02-07 16:07:09 +0800

    ...

---
+++ pbc/pbc_dkscbjll.py
@@ -0,0 +1,40 @@
+import time
+
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options
+
+# 贷款市场报价利率（LPR）
+options = Options()
+options.add_argument('--headless')
+driver = Chrome(executable_path='../chromedriver.exe', options=options)
+
+try:
+    driver.get('http://www.pbc.gov.cn/zhengcehuobisi/125207/125213/125440/3876551/index.html')
+    totalpage = driver.find_element_by_id("r_con").find_element_by_tag_name('input').get_attribute('totalpage')
+    page = int(totalpage)
+
+    table_tag = \
+        driver.find_element_by_id("r_con").find_element_by_tag_name('tbody').find_elements_by_tag_name('tr')[
+            1].find_element_by_tag_name('td').find_elements_by_tag_name('table')
+
+    for t in table_tag:
+        a_tag = t.find_element_by_tag_name('a')
+        span_tag = t.find_element_by_tag_name('span')
+        a_tag_href = t.find_element_by_tag_name('a').get_attribute('href')
+        print(a_tag.text + '\u0020' + span_tag.text + '：' + a_tag_href)
+
+# for p in range(1, page + 1):
+#     driver.get('http://www.pbc.gov.cn/zhengcehuobisi/125207/125213/125431/125475/17081/index' + str(p) + '.html')
+#     table_tag = \
+#         driver.find_element_by_id("r_con").find_element_by_tag_name('tbody').find_elements_by_tag_name('tr')[
+#             1].find_element_by_tag_name('td').find_elements_by_tag_name('table')
+#     for t in table_tag:
+#         a_tag = t.find_element_by_tag_name('a')
+#         span_tag = t.find_element_by_tag_name('span')
+#         a_tag_href = t.find_element_by_tag_name('a').get_attribute('href')
+#         print(a_tag.text + '\u0020' + span_tag.text + '：' + a_tag_href)
+#     time.sleep(2)
+
+    driver.quit()
+except:
+    driver.quit()
